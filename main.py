549711f932c368b8471509e329a83427442698bf
Matías Ortiz <matiasortizurrutia0@gmail.com>
2025-01-15 23:04:21 -0400

    websocket port changed

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import webview
 import multiprocessing
 
-def run_webview(debug):
-    window = webview.create_window('My Webview App', './src/index.html', width=1500, height=1000)
+def run_webview():
+    # window = webview.create_window('GamerStation', './src/index.html', width=1500, height=1000)
+    window = webview.create_window('GamerStation', './src/index.html', fullscreen=True)
     webview.start(gui='qt', icon='./favicon.ico')
 
 if __name__ == "__main__":
-    debug = False
-
-    try:
-        webview_thread = multiprocessing.Process(target=run_webview, args=(debug,))
-        webview_thread.start()
-        webview_thread.join()
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    webview_thread = multiprocessing.Process(target=run_webview)
+    webview_thread.start()
+    webview_thread.join()
